a6777ffe35e6aae4efc54e9a5f8287b7b6fb642a
DeBraid <de.braid@gmail.com>
2014-02-28 15:13:32 -0500

    added more depth to data, cleaned up old files

--- styles/js/d3nw.js
+++ styles/js/d3nw.js
@@ -1,8 +1,5 @@
-console.log("BAZZINGTONZZ");
-
-d3.csv('canadaNW.csv', function(data){
-  var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
+  var margin = {top: 20, right: 20, bottom: 150, left: 60},
+    width = 800 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
   var x0 = d3.scale.ordinal()
@@ -13,8 +10,12 @@ d3.csv('canadaNW.csv', function(data){
   var y = d3.scale.linear()
       .range([height, 0]);
 
+// var years = new Array("pc2005","pc2012");
+  var years = new Array("1999","2005","2012");
+
   var color = d3.scale.ordinal()
-      .range(["#98abc5", "#8a89a6", "#7b6888"]);
+      .domain(years)
+      .range(["#FF3333", "#FF6666", "#FF9999"]);
 
   var xAxis = d3.svg.axis()
       .scale(x0)
@@ -23,7 +24,12 @@ d3.csv('canadaNW.csv', function(data){
   var yAxis = d3.svg.axis()
       .scale(y)
       .orient("left")
-      .tickFormat(d3.format(".2s"));
+      .tickFormat(d3.format("$.1s"));
+
+
+
+d3.csv('canadaNWonly.csv', function(data){
+  
 
   var svg = d3.select("#svg-chart").append("svg")
       .attr("width", width + margin.left + margin.right)
@@ -31,20 +37,27 @@ d3.csv('canadaNW.csv', function(data){
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  var years = d3.keys(data[0]).filter(function(key) { return key !== "Type"; });
-
     data.forEach(function(d) {
       d.groups = years.map(function(year) { return {year: year, total: +d[year]}; });
     });
 
+
     x0.domain(data.map(function(d) { return d.Type; }));
     x1.domain(years).rangeRoundBands([0, x0.rangeBand()]);
     y.domain([0, d3.max(data, function(d) { return d3.max(d.groups, function(d) { return d.total; }); })]);
-
+    
+    data.sort(function ( a,b ){ return a.total > b.total });
+    
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        .call(xAxis)
+        .selectAll("text").style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", function(d) {
+                  return "rotate(-45)" 
+                });
 
     svg.append("g")
         .attr("class", "y axis")
@@ -54,7 +67,7 @@ d3.csv('canadaNW.csv', function(data){
         .attr("y", 6)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
-        .text("Population");
+        .text("$ in millions");
 
     var type = svg.selectAll(".type")
         .data(data)
@@ -70,6 +83,25 @@ d3.csv('canadaNW.csv', function(data){
         .attr("y", function(d) { return y(d.total); })
         .attr("height", function(d) { return height - y(d.total); })
         .style("fill", function(d) { return color(d.year); });
+
+    var legend = svg.selectAll(".legend")
+        .data(years.slice().reverse())
+      .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+    legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
+
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function(d) { return d; });
 
 
 })
