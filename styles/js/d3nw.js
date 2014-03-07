5badfd7324f3a4f9236a7bdd03b944ae33a488d9
DeBraid <de.braid@gmail.com>
2014-03-07 18:02:10 -0500

    horizontal chart up, very basic

--- styles/js/d3nw.js
+++ styles/js/d3nw.js
@@ -1,107 +1,85 @@
-  var margin = {top: 20, right: 20, bottom: 150, left: 60},
-    width = 800 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+  var valueLabelWidth = 40; // space reserved for value labels (right)
+var barHeight = 20; // height of one bar
+var barLabelWidth = 150; // space reserved for bar labels
+var barLabelPadding = 10; // padding between bar and bar labels (left)
+var gridLabelHeight = 25; // space reserved for gridline labels
+var gridChartOffset = 10; // space between start of grid and first bar
+var maxBarWidth = 420; // width of the bar with the max value
+ 
+d3.csv('canadaNWonly.csv', function(data){  
+ 
+// accessor functions 
+var barLabel = function(d) { return d['Type']; };
+var barValue = function(d) { return parseFloat(d['pc2012']); };
+ 
+// sorting
+var sortedData = data.sort(function(a, b) {
+ return d3.descending(barValue(a), barValue(b));
+}); 
+console.log (data);
+// scales
+var yScale = d3.scale.ordinal().domain(d3.range(0, sortedData.length)).rangeBands([0, sortedData.length * barHeight]);
+var y = function(d, i) { return yScale(i); };
+var yText = function(d, i) { return y(d, i) + yScale.rangeBand() / 2; };
+var x = d3.scale.linear().domain([0, d3.max(sortedData, barValue)]).range([0, maxBarWidth]);
 
-  var x0 = d3.scale.ordinal()
-      .rangeRoundBands([0, width], .1);
+// svg container element
+var chart = d3.select('#svg-chart').append("svg")
+  .attr('width', maxBarWidth + barLabelWidth + valueLabelWidth)
+  .attr('height', gridLabelHeight + gridChartOffset + sortedData.length * barHeight);
 
-  var x1 = d3.scale.ordinal();
+// grid line labels
+var gridContainer = chart.append('g')
+  .attr('transform', 'translate(' + barLabelWidth + ',' + gridLabelHeight + ')'); 
 
-  var y = d3.scale.linear()
-      .range([height, 0]);
+gridContainer.selectAll("text").data(x.ticks(10)).enter().append("text")
+  .attr("x", x)
+  .attr("dy", -3)
+  .attr("text-anchor", "middle")
+  .text(String);
 
-// var years = new Array("pc2005","pc2012");
-  var years = new Array("1999","2005","2012");
+// vertical grid lines
+gridContainer.selectAll("line").data(x.ticks(3)).enter().append("line")
+  .attr("x1", x)
+  .attr("x2", x)
+  .attr("y1", 0)
+  .attr("y2", yScale.rangeExtent()[1] + gridChartOffset)
+  .style("stroke", "gray");
 
-  var color = d3.scale.ordinal()
-      .domain(years)
-      .range(["#FF3333", "#FF6666", "#FF9999"]);
-
-  var xAxis = d3.svg.axis()
-      .scale(x0)
-      .orient("bottom");
-
-  var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left")
-      .tickFormat(d3.format("$.1s"));
-
-
-
-d3.csv('canadaNWonly.csv', function(data){
+// bar labels
+var labelsContainer = chart.append('g')
+  .attr('transform', 'translate(' + (barLabelWidth - barLabelPadding) + ',' + (gridLabelHeight + gridChartOffset) + ')'); 
+labelsContainer.selectAll('text').data(sortedData).enter().append('text')
+  .attr('y', yText)
+  .attr('fill', 'Red')
+  .attr("dy", ".35em") // vertical-align: middle
+  .attr('text-anchor', 'end')
+  .text(barLabel);
+// bars
+var barsContainer = chart.append('g')
+  .attr('transform', 'translate(' + barLabelWidth + ',' + (gridLabelHeight + gridChartOffset) + ')'); 
+barsContainer.selectAll("rect").data(sortedData).enter().append("rect")
+  .attr('y', y)
+  .attr('height', yScale.rangeBand())
+  .attr('width', function(d) { return x(barValue(d)); })
+  .attr('stroke', 'red')
+  .attr('fill', 'tomato');
   
+// bar value labels
+barsContainer.selectAll("text").data(sortedData).enter().append("text")
+  .attr("x", function(d) { return x(barValue(d)); })
+  .attr("y", yText)
+  .attr("dx", 5) // padding-left
+  .attr("dy", ".35em") // vertical-align: middle
+  .attr("text-anchor", "start") // text-align: right
+  .attr("fill", "black")
+  .attr("stroke", "none")
+  .text(function(d) { return d3.round(barValue(d), 1); });
 
-  var svg = d3.select("#svg-chart").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+// start line
+barsContainer.append("line")
+  .attr("y1", -gridChartOffset)
+  .attr("y2", yScale.rangeExtent()[1] + gridChartOffset)
+  .style("stroke", "black");
 
-    data.forEach(function(d) {
-      d.groups = years.map(function(year) { return {year: year, total: +d[year]}; });
-    });
-
-
-    x0.domain(data.map(function(d) { return d.Type; }));
-    x1.domain(years).rangeRoundBands([0, x0.rangeBand()]);
-    y.domain([0, d3.max(data, function(d) { return d3.max(d.groups, function(d) { return d.total; }); })]);
-    
-    data.sort(function ( a,b ){ return a.total > b.total });
-    
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        .selectAll("text").style("text-anchor", "end")
-            .attr("dx", "-.8em")
-            .attr("dy", ".15em")
-            .attr("transform", function(d) {
-                  return "rotate(-45)" 
-                });
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-      .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("$ in millions");
-
-    var type = svg.selectAll(".type")
-        .data(data)
-      .enter().append("g")
-        .attr("class", "g")
-        .attr("transform", function(d) { return "translate(" + x0(d.Type) + ",0)"; });
-
-    type.selectAll("rect")
-        .data(function(d) { return d.groups; })
-      .enter().append("rect")
-        .attr("width", x1.rangeBand())
-        .attr("x", function(d) { return x1(d.year); })
-        .attr("y", function(d) { return y(d.total); })
-        .attr("height", function(d) { return height - y(d.total); })
-        .style("fill", function(d) { return color(d.year); });
-
-    var legend = svg.selectAll(".legend")
-        .data(years.slice().reverse())
-      .enter().append("g")
-        .attr("class", "legend")
-        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-    legend.append("rect")
-        .attr("x", width - 18)
-        .attr("width", 18)
-        .attr("height", 18)
-        .style("fill", color);
-
-    legend.append("text")
-        .attr("x", width - 24)
-        .attr("y", 9)
-        .attr("dy", ".35em")
-        .style("text-anchor", "end")
-        .text(function(d) { return d; });
-
-
-})
+});
